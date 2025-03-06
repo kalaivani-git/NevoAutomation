fca41b771eadc2e32802c56e1c0f76ad390b28ac
kalaivani-git <kalaithanigachalam@gmail.com>
2025-03-06 11:18:07 +0530

    Update TestDataUtility.cs

--- Data/TestDataUtility.cs
+++ Data/TestDataUtility.cs
@@ -1,18 +1,3 @@
-﻿#region File Header
-/*  ----------------------------------------------------------------------------
- *  NibrasUTTest Automation : ConcertIDC
- *  ----------------------------------------------------------------------------
- *  ----------------------------------------------------------------------------
- *  File:
- *  Author:    Nagabhushana A
- *  Reworker: 
- *  Description : TestData Utility created
- *  Creation Date: 17-11-2023
- *  Modified Date: 
- *  ----------------------------------------------------------------------------
- */
-#endregion
-
 namespace Nevo.Data
 {
     public class TestDataUtility
