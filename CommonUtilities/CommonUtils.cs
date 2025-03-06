c2a8541428f8c296c351db6ea633eee075431213
kalaivani-git <kalaithanigachalam@gmail.com>
2025-03-06 11:14:11 +0530

    Update CommonUtils.cs

--- CommonUtilities/CommonUtils.cs
+++ CommonUtilities/CommonUtils.cs
@@ -1,17 +1,3 @@
-﻿#region File Header
-/*  ----------------------------------------------------------------------------
- *  NibrasUTTest Automation : ConcertIDC
- *  ----------------------------------------------------------------------------
- *  ----------------------------------------------------------------------------
- *  File:
- *  Author:    Nagabhushana A
- *  Reworker: 
- *  Description : Common utility Utility created
- *  Creation Date: 17-11-2023
- *  Modified Date: 
- *  ----------------------------------------------------------------------------
- */
-#endregion
 using AventStack.ExtentReports;
 using Nevo.HelperUtilities;
 using OpenQA.Selenium;
